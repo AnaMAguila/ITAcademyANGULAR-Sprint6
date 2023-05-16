7c383341c5fdc40d5dcb48c3518d83cfc9c1b1f9
Ana María Águila <ana_mari_aguila@hotmail.com>
2023-05-16 17:09:50 +0200

    Exercici 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  dataFrases: any[];
 
+  constructor(){
+    this.dataFrases = [
+      {
+        "id" : 0,
+        "frase" : "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+      },
+      {
+        "id" : 1,
+        "frase" : "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes."
+      },
+      {
+        "id" : 2,
+        "frase" : "L'heroi va decidir travessar la porta que el portava a casa"
+      },
+      {
+        "id" : 3,
+        "frase" : "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
+      },
+    ]
+  }
 }
