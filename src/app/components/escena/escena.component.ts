5313bca88e5d46ed9e62a1682b7009271b04ba87
Ana María Águila <ana_mari_aguila@hotmail.com>
2023-05-18 11:26:35 +0200

    Exercici 4

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -7,4 +7,21 @@ import { Component, Input } from '@angular/core';
 })
 export class EscenaComponent {
   @Input() frase : any;
+  public currentSentence = 0;
+
+  prev(){
+    if(this.currentSentence>0){
+      this.currentSentence--;
+    }else if(this.currentSentence<0){
+      this.currentSentence = 0;
+    }
+  }
+
+  next(){
+    if(this.currentSentence<3){
+      this.currentSentence++;
+    }else if(this.currentSentence>3){
+        this.currentSentence = 3;
+    }
+  }
 }
