4e0fdc0bf308941c905395d1545e18debee9a64c
Ana María Águila <ana_mari_aguila@hotmail.com>
2023-06-09 10:15:13 +0200

    Exercici 6

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,19 +13,23 @@ export class HomeComponent {
     this.dataFrases = [
       {
         "id" : 0,
-        "frase" : "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+        "txt" : "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+        "img" : "1.jpg"
       },
       {
         "id" : 1,
-        "frase" : "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes."
+        "txt" : "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+        "img" : "2.jpg"
       },
       {
         "id" : 2,
-        "frase" : "L'heroi va decidir travessar la porta que el portava a casa"
+        "txt" : "L'heroi va decidir travessar la porta que el portava a casa",
+        "img" : "3.jpg"
       },
       {
         "id" : 3,
-        "frase" : "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ..."
+        "txt" : "Mentrestant, altres heroes no van tenir tanta sort en la seva elecció ...",
+        "img" : "4.jpg"
       },
     ]
   }
